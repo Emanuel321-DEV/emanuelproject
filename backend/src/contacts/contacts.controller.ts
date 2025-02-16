8cc3c9f663cb9b70b9ab23b1fa91ff9c076fe240
emanuel@wcore.com.br <emanuel@wcore.com.br>
2025-02-16 18:11:15 -0300

    feat: change path import jwt guard

--- backend/src/contacts/contacts.controller.ts
+++ backend/src/contacts/contacts.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('contacts')
 @UseGuards(JwtAuthGuard)
